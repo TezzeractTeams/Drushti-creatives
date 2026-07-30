132102388679d21dd413bce479da48c667ce6026
Oneli-Kumarasinghe <onelisaku@gmail.com>
2026-07-30 07:43:51 +0530

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,7 +3,6 @@ import ServicesHero from "@/sections/ServicesHero";
 import ServicesShowcase from "@/sections/ServicesShowcase";
 import WorkProcess from "@/sections/WorkProcess";
 import Testimonials from "@/sections/Testimonials";
-import SubServicesCarousel from "@/sections/subservices";
 
 export const metadata: Metadata = {
   title: "Services | Drushti Creatives",
